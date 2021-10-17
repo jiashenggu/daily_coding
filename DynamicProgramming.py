7f0be7b281d3aa293cb686ae2c19f3016e833a1a
Sieg-Arash <984931887@qq.com>
2021-10-16 23:15:55 -0700

    10.16

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -1,3 +1,37 @@
+# 188. Best Time to Buy and Sell Stock IV
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        n = len(prices)
+        k = min(k, n // 2)
+        buy = [(k + 1) * [0] for _ in range(n)]
+        sell = [(k + 1) * [0] for _ in range(n)]
+        buy[0][0], sell[0][0] = -prices[0], 0
+        for i in range(1, k + 1):
+            buy[0][i], sell[0][i] = float("-inf"), float("-inf")
+        for i in range(1, n):
+            buy[i][0] = max(buy[i - 1][0], sell[i - 1][0] - prices[i])
+            for j in range(1, k + 1):
+                buy[i][j] = max(buy[i - 1][j], sell[i - 1][j] - prices[i])
+                sell[i][j] = max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i])
+        return max(sell[n - 1])
+
+
+# 309. Best Time to Buy and Sell Stock with Cooldown
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sold, held, reset = float("-inf"), float("-inf"), 0
+
+        for price in prices:
+            pre_sold = sold
+            sold = held + price
+            held = max(held, reset - price)
+            reset = max(reset, pre_sold)
+
+        return max(reset, sold)
+
+
 # 174. Dungeon Game
 class Solution(object):
     def calculateMinimumHP(self, dungeon):
