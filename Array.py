fa938e2fb9eb6c5937d17eaf6b61f897a03bb896
Sieg-Arash <984931887@qq.com>
2021-11-21 17:51:53 -0800

    11.21

--- Array.py
+++ Array.py
@@ -1,3 +1,25 @@
+# 384. Shuffle an Array
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = nums.copy()
+
+    def reset(self) -> List[int]:
+        self.nums = self.original.copy()
+        return self.nums
+    def shuffle(self) -> List[int]:
+        for i in range(len(self.nums)):
+            j = random.randrange(i, len(self.nums))
+            self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
+        return self.nums
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
+
 # 4. Median of Two Sorted Arrays
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
