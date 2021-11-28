37201bec16b07ae6113ddb2e0d29b064463d7dc2
Sieg-Arash <984931887@qq.com>
2021-11-28 13:44:50 -0800

    11.21

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,3 +1,28 @@
+# 786. K-th Smallest Prime Fraction
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        n = len(arr)
+        l, r = 0.0, 1.0
+
+        while True:
+            mid = (l + r) / 2
+            i = -1
+            count = 0
+            x, y = 0, 1
+            for j in range(n):
+                while arr[i + 1] / arr[j] < mid:
+                    i += 1
+                    if arr[i] * y > arr[j] * x:
+                        x, y = arr[i], arr[j]
+                count += i + 1
+            if count == k:
+                return [x, y]
+            elif count > k:
+                r = mid
+            else:
+                l = mid
+
+
 # 1231. Divide Chocolate
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
