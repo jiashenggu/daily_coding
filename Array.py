234ea51ec676b2909621e5789d8c2dc28e7a7fb6
Sieg-Arash <984931887@qq.com>
2021-12-23 20:55:38 -0800

    12.23

--- Array.py
+++ Array.py
@@ -1,3 +1,20 @@
+# 41. First Missing Positive
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            if nums[i] <= 0:
+                nums[i] = n + 1
+
+        for i in range(n):
+            a = abs(nums[i])
+            if 1 <= a <= n:
+                nums[a - 1] = -abs(nums[a - 1])
+
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+        return n + 1
 # 384. Shuffle an Array
 class Solution:
 
--- Tree.py
+++ Tree.py
@@ -1,3 +1,55 @@
+# 297. Serialize and Deserialize Binary Tree
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return "None,"
+        s = ""
+        s += str(root.val) + ','
+        s += self.serialize(root.left)
+        s += self.serialize(root.right)
+        print(s)
+        return s
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        data_list = data.split(',')
+        # print(data_list)
+        return self.dfs(data_list)
+
+    def dfs(self, l):
+        if not l:
+            return None
+        val = l.pop(0)
+        if val == 'None':
+            return None
+        root = TreeNode(val)
+        root.left = self.dfs(l)
+        root.right = self.dfs(l)
+        return root
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
 # 450. Delete Node in a BST
 # Definition for a binary tree node.
 # class TreeNode:
