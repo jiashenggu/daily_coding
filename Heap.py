7087c850696958d284c5c803b1711597012b30be
Sieg-Arash <984931887@qq.com>
2022-01-01 01:01:38 -0800

    12.31

--- Heap.py
+++ Heap.py
@@ -1,3 +1,38 @@
+# 352. Data Stream as Disjoint Intervals
+class SummaryRanges(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.intervals = []
+        self.seen = set()
+
+    def addNum(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if val not in self.seen:
+            self.seen.add(val)
+            heapq.heappush(self.intervals, [val, val])
+
+    def getIntervals(self):
+        """
+        :rtype: List[List[int]]
+        """
+
+        tmp = []
+
+        while self.intervals:
+            cur = heapq.heappop(self.intervals)
+            if tmp and cur[0] <= tmp[-1][1] + 1:
+                tmp[-1][1] = max(tmp[-1][1], cur[1])
+            else:
+                tmp.append(cur)
+
+        self.intervals = tmp
+        return self.intervals
 # 295. Find Median from Data Stream
 class MedianFinder:
     def __init__(self):
