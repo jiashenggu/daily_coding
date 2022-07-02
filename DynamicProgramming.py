cb423aa38558b985f15981405b37eb88c20ebe60
Sieg-Arash <984931887@qq.com>
2022-07-01 19:35:12 -0700

    7/1/2022

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -1,3 +1,25 @@
+# 256. Paint House
+import copy
+
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+
+        if len(costs) == 0: return 0
+
+        previous_row = costs[0]
+        for n in range(1, len(costs)):
+
+            current_row = copy.deepcopy(costs[n])
+            # Total cost of painting nth house red?
+            current_row[0] += min(previous_row[1], previous_row[2])
+            # Total cost of painting nth house green?
+            current_row[1] += min(previous_row[0], previous_row[2])
+            # Total cost of painting nth house blue?
+            current_row[2] += min(previous_row[0], previous_row[1])
+            previous_row = current_row
+
+        return min(previous_row)
+
 # 968. Binary Tree Cameras
 # Definition for a binary tree node.
 # class TreeNode:
--- Sort.py
+++ Sort.py
@@ -1,3 +1,37 @@
+# 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+class Solution:
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        # Start by sorting the inputs
+        horizontalCuts.sort()
+        verticalCuts.sort()
+
+        # Consider the edges first
+        max_height = max(horizontalCuts[0], h - horizontalCuts[-1])
+        for i in range(1, len(horizontalCuts)):
+            # horizontalCuts[i] - horizontalCuts[i - 1] represents the distance between
+            # two adjacent edges, and thus a possible height
+            max_height = max(max_height, horizontalCuts[i] - horizontalCuts[i - 1])
+
+        # Consider the edges first
+        max_width = max(verticalCuts[0], w - verticalCuts[-1])
+        for i in range(1, len(verticalCuts)):
+            # verticalCuts[i] - verticalCuts[i - 1] represents the distance between
+            # two adjacent edges, and thus a possible width
+            max_width = max(max_width, verticalCuts[i] - verticalCuts[i - 1])
+
+        # Python doesn't need to worry about overflow - don't forget the modulo though!
+        return (max_height * max_width) % (10 ** 9 + 7)
+# 1710. Maximum Units on a Truck
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key = lambda x: -x[1])
+        ans = 0
+        i = 0
+        while i<len(boxTypes) and truckSize>0:
+            ans += min(truckSize, boxTypes[i][0])*boxTypes[i][1]
+            truckSize -= boxTypes[i][0]
+            i+=1
+        return ans
 # 462. Minimum Moves to Equal Array Elements II
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
