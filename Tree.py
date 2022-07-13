4d0d4fb772213cbc007e3641974844b592943954
Sieg-Arash <984931887@qq.com>
2022-07-12 18:50:20 -0700

    7/12/2022

--- Tree.py
+++ Tree.py
@@ -1,3 +1,29 @@
+# 102. Binary Tree Level Order Traversal
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = collections.deque([root])
+        ans = []
+        while q:
+            nq = len(q)
+            tmp = []
+            for _ in range(nq):
+                cur = q.popleft()
+                tmp.append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            ans.append(tmp)
+        return ans
+
 # 199. Binary Tree Right Side View
 # Definition for a binary tree node.
 # class TreeNode:
